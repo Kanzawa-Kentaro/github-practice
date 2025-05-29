df1e3154306943a136fc7321d31a4098a3e48601
kanzawa <2119077b@gmail.com>
2025-05-29 23:37:46 +0900

    型を学びました

---
+++ JAVA/selfjava/src/to/msn/wings/selfjava/chap01/Practice3.java
@@ -0,0 +1,21 @@
+package to.msn.wings.selfjava.chap01;
+
+public class Practice3 {
+    public static void main(String[] args) {
+
+    var value = 10d;
+
+    System.out.println("""
+    ようこそ
+    Javaの世界へ!""");
+
+    String str = null;
+
+    int[][] data = new int[5][4];
+    var list = new int[][]{
+      {2, 3, 5},
+      {1, 2},
+      {10, 11, 12, 13},
+    };
+  }
+}
